0919dffa0645e38f3af9e5bec8774983f3b7ec1a
mgmenezes <mgmenezes>
2021-04-14 11:56:20 -0300

    Ajustes

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,26 @@
+
+import logoImg from '../../assets/logo.svg';
+
+import { Container, Content } from './styles';
+
+interface HeaderProps{
+  onOpenNewTransactionModal: () => void;
+}
+
+ // eslint-disable-next-line
+export function Header({onOpenNewTransactionModal}: HeaderProps) {
+
+    return(
+    <Container>
+     <Content>
+        <img src={logoImg} alt="dt money"/>    
+        
+        <button type="button" onClick ={onOpenNewTransactionModal}>
+            Nova transação
+        </button> 
+
+      </Content>
+    </Container>
+
+    )
+}
